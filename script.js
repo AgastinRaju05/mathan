42e5286525a3fad0a9a0bd5f3ddc45a2e19df1ce
AgastinRaju05 <agastinraju08@gmail.com>
2025-02-17 10:16:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("showMore").addEventListener("click", function() {
+    const extraInfo = document.getElementById("extraInfo");
+    if (extraInfo.classList.contains("hidden")) {
+        extraInfo.classList.remove("hidden");
+        this.textContent = "Show Less";
+    } else {
+        extraInfo.classList.add("hidden");
+        this.textContent = "Learn More";
+    }
+});
